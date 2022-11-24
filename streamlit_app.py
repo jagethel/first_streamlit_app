c23e35cc555e533d9bae9da4d24cd3ef3aef3639
MellyMel Melnyk <118989821+jagethel@users.noreply.github.com>
2022-11-24 08:17:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 'BREAKFAST MENU'
+
+'Omega 3 & Blueberry Oatmeal'
+
+'Kale, Spinach & Rocket Smoothie'
+
+'Hard-Boiled and Free-Range Egg'
